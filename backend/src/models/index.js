f4f05413f73d5c30b898536e6ee9408b36914471
BhavishwaReddy2023 <bhavishwareddy5@gmail.com>
2025-09-07 18:51:11 +0530

    schema cretead

--- backend/src/models/index.js
+++ backend/src/models/index.js
@@ -1,7 +1,22 @@
 const Teacher = require('./Teacher');
 const Student = require('./Student');
+const Classroom = require('./Classroom');
+const Material = require('./Material');
+const Assignment = require('./Assignment');
+const Submission = require('./Submission');
+const Doubt = require('./Doubt');
+const { Badge, Milestone } = require('./Badge');
+const Leaderboard = require('./Leaderboard');
 
 module.exports = {
   Teacher,
-  Student
+  Student,
+  Classroom,
+  Material,
+  Assignment,
+  Submission,
+  Doubt,
+  Badge,
+  Milestone,
+  Leaderboard
 };
